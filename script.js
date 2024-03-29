aefc9b2e30d78ad176f6accdb8d0860b827ad5e3
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-03-29 19:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateText() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+updateText().then((message) => {
+  document.getElementById('output').innerText = message;
+});
